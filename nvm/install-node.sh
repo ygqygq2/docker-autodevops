62d098b2783459faaa124bbd1088fca570d469fc
ygqygq2 <ygqygq2@qq.com>
2025-01-10 18:45:43 +0800

    feat: shell install nvm
    Co-authored-by: ygqygq2 <ygqygq2@qq.com>

--- nvm/install-node.sh
+++ nvm/install-node.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
+set -e
+
+# 有接 $1 参数则安装 node，否则只安装 nvm
 NVM_DIR=${NVM_DIR:-"/root/.nvm"}
-TO_INSTALL=${1:-"--lts"}
+TO_INSTALL=${1}
 
 # 安装 NVM
 mkdir -p $NVM_DIR
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash -x
 echo "Installed NVM"
 
-# 安装指定版本的 Node.js
+# 加载 NVM
 source $NVM_DIR/nvm.sh
-for version in $(echo $TO_INSTALL | tr "," "\n"); do
-  nvm install $version
-done
+
+# 如果有指定 Node.js 版本，则安装
+if [ -n "$TO_INSTALL" ]; then
+  for version in $(echo $TO_INSTALL | tr "," "\n"); do
+    nvm install $version
+  done
+
+  # 清理 NVM 缓存
+  rm -rf $NVM_DIR/.cache
+
+  echo "Node.js versions installed: $(nvm ls)"
+else
+  echo "NVM installed, but no Node.js version specified."
+fi
